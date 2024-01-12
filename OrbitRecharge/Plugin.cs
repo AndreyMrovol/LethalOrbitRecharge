9b41a118037df345b22d845c0f686527dec3a673
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-01-12 13:55:11 +0100

    first version

---
+++ OrbitRecharge/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace OrbitRecharge
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(StartOfRound))]
+    public static class StartOfRound_SetShipReadyToLand_Patch
+    {
+        [HarmonyPatch("SetShipReadyToLand")]
+        [HarmonyPrefix]
+        public static void Prefix(StartOfRound __instance)
+        {
+            GameObject ship = GameObject.Find("/Environment/HangarShip");
+            var ItemsOnShip = ship.GetComponentsInChildren<GrabbableObject>();
+
+            foreach (var item in ItemsOnShip)
+            {
+                if (item.insertedBattery != null)
+                {
+                    item.insertedBattery = new Battery(false, 1f);
+                    item.SyncBatteryServerRpc(100);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# OrbitRecharge
+
+Automatically recharge all items on ship when going into orbit.
+
+## Installation
+
+Copy **`OrbitRecharge.dll`** to your BepInEx plugins folder.
+
+\- or -
+
+Install via r2modman.
+
+## License
+
+This project is licensed under [MIT License](https://github.com/AndreyMrovol/LethalOrbitRecharge/blob/main/LICENSE).
+
+## Credits
+
+This project uses [LethalCompanyTemplate](https://github.com/LethalCompany/LethalCompanyTemplate), licensed under [MIT License](https://github.com/LethalCompany/LethalCompanyTemplate/blob/main/LICENSE).
