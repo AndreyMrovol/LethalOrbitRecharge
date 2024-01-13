57c210e7482bbf68336ff2f636c2c635caad8720
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-01-13 03:09:02 +0100

    charge items in players' inventories

--- OrbitRecharge/Plugin.cs
+++ OrbitRecharge/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 
@@ -7,10 +8,15 @@ namespace OrbitRecharge
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+
+        internal static ManualLogSource logger;
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            logger = Logger;
 
             var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
@@ -20,6 +26,13 @@ namespace OrbitRecharge
     [HarmonyPatch(typeof(StartOfRound))]
     public static class StartOfRound_SetShipReadyToLand_Patch
     {
+
+        private static void RechargeItem(GrabbableObject itemToCharge)
+        {
+            itemToCharge.insertedBattery = new Battery(false, 1f);
+            itemToCharge.SyncBatteryServerRpc(100);
+        }
+
         [HarmonyPatch("SetShipReadyToLand")]
         [HarmonyPrefix]
         public static void Prefix(StartOfRound __instance)
@@ -31,8 +44,21 @@ namespace OrbitRecharge
             {
                 if (item.insertedBattery != null)
                 {
-                    item.insertedBattery = new Battery(false, 1f);
-                    item.SyncBatteryServerRpc(100);
+                    RechargeItem(item);
+                }
+            }
+
+            var Players = __instance.allPlayerScripts;
+            foreach (var player in Players)
+            {
+                GrabbableObject[] ItemsOnPlayer = player.GetComponentsInChildren<GrabbableObject>();
+
+                foreach (var item in ItemsOnPlayer)
+                {
+                    if (item.insertedBattery != null)
+                    {
+                        RechargeItem(item);
+                    }
                 }
             }
         }
